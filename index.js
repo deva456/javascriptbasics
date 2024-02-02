f17b5112084af53679d61facf12ee3235f299150
deva456 <devashishkapadnis075@gmail.com>
2024-02-02 13:29:56 +0530

    js  basics

---
+++ index.js
@@ -0,0 +1,69 @@
+// 1)
+//let name = 'Nashik';
+//console.log(name);
+
+// cannot be a reserved keyword 
+// Should be meaningful
+// Cannot start with a number (1name)
+// Cannot contain a space or hyphen (-)
+// Are case-sensitive 
+
+// 2)
+// let name = 'Raj'; //string literal
+// let age = 30;  // number literal
+// let isApproved = false; // boolean literal
+// let firstName = undefined;
+// let selectedColor = null;
+
+//let firstName = 'Ajay';
+
+//let lastName = 'Patil';
+
+
+// 3)
+ 
+
+// let person  = {
+//     name: 'Devashish',
+//     age: 27
+// };
+// //Dot annotation 
+// person.name = 'John';
+
+// //Bracket annotation
+// let selection = 'name';
+// person[selection] = 'Mary';
+
+// let selectedColors = ['red','blue']
+// selectedColors[2]='1';
+// console.log(selectedColors);
+// console.log(selectedColors.length);
+// console.log(selectedColors[1]);
+//console.log(person);
+
+//4)
+
+function greet() {
+    console.log('Nashik is Best');
+}
+
+// function greet(name) {
+//     console.log('Hello ' + name);
+// }
+
+// 5)
+// Performing a task
+//function greet(name, lastName) {
+   // console.log('Hello ' + name + ' ' + lastName);
+//}
+//greet('Devashish','Kapadnis');
+
+
+//calculating a value 
+function square(number){
+    return number * number;
+}
+
+let number = square(2);
+console.log(number);
+
